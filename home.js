d419a670e47425e966a50208ea33996f33345fa0
haru207toku <150308539+haru207toku@users.noreply.github.com>
2024-07-16 10:07:11 +0900

    Create home.js

---
+++ home.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startBtn = document.getElementById('startBtn');
+
+    startBtn.addEventListener('click', () => {
+        window.location.href = 'avocado.html'; // weather.htmlに遷移
+    });
+});
